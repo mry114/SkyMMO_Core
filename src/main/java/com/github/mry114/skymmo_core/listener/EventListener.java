dc81a59d4fe8e362ff28400a94ace43d3467c962
hum1qwq <161867086+hum1qwq@users.noreply.github.com>
2026-06-05 00:11:37 +0900

    ちょっと手直し

--- src/main/java/com/github/mry114/skymmo_core/listener/EventListener.java
+++ src/main/java/com/github/mry114/skymmo_core/listener/EventListener.java
@@ -1,17 +1,15 @@
 package com.github.mry114.skymmo_core.listener;
 
-import com.github.mry114.skymmo_core.api.item.ICustomItem;
 import com.github.mry114.skymmo_core.api.item.capa.ICustomItemSkill;
-import com.github.mry114.skymmo_core.api.item.equipment.IEquipmentItem;
 import com.github.mry114.skymmo_core.core.context.ItemGeneratorContext;
 import com.github.mry114.skymmo_core.core.item.factory.CustomItemFactory;
 import com.github.mry114.skymmo_core.data.context.attribute.ItemAttributeModuleKeys;
 import com.github.mry114.skymmo_core.data.context.basic.MainModuleKeys;
+import com.github.mry114.skymmo_core.data.model.attribute.ExampleAttribute;
 import com.github.mry114.skymmo_core.data.model.attribute.Mysterious;
 import com.github.mry114.skymmo_core.registry.ItemRegistry;
 import com.github.mry114.skymmo_core.util.pdc.PDCWrapper;
 import io.papermc.paper.event.player.AsyncChatEvent;
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -30,9 +28,9 @@ public class EventListener implements Listener {
                 Player player = event.getPlayer();
 
                 ItemGeneratorContext context = new ItemGeneratorContext();
-                context.put(ItemAttributeModuleKeys.ITEM_ATTRIBUTE, new Mysterious());
+                context.put(ItemAttributeModuleKeys.ITEM_ATTRIBUTE, new ExampleAttribute());
 
-                player.getInventory().setItem(0, new CustomItemFactory(ItemRegistry.HYPERION).create(context));
+                player.getInventory().setItem(0, new CustomItemFactory(ItemRegistry.EXAMPLE_WEAPON).create(context));
             }
         }
     }
